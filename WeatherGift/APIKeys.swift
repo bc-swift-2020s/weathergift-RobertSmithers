9716b2560dc1d148fda47da6f8b29bec535b32f0
RobertSmithers <rjsmithers3@gmail.com>
2020-03-16 15:58:05 -0400

    Completed 7.3 and 7.4

---
+++ WeatherGift/APIKeys.swift
@@ -0,0 +1,13 @@
+//
+//  APIKeys.swift
+//  WeatherGift
+//
+//  Created by RJ Smithers on 3/16/20.
+//  Copyright © 2020 RJ Smithers. All rights reserved.
+//
+
+import Foundation
+
+struct APIKeys {
+    static let googlePlacesKey = "AIzaSyCerL5eYQfl6vt2xhKeIMxF177WQyzvUSs"
+}
