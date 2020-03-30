61e8cae231d37d17466240fcc014fc8e26ed1b54
RobertSmithers <rjsmithers3@gmail.com>
2020-03-30 13:54:51 -0400

    Completed 7.14

--- WeatherGift/WeatherDetail.swift
+++ WeatherGift/WeatherDetail.swift
@@ -8,20 +8,42 @@
 
 import UIKit
 
+private let dateFormatter: DateFormatter = {
+    let dateFormatter = DateFormatter()
+    dateFormatter.dateFormat = "EEE"
+    return dateFormatter
+}()
+
+struct DailyWeatherData: Codable {
+    var dailyIcon: String
+    var dailyWeekday: String
+    var dailySummary: String
+    var dailyHigh: Int
+    var dailyLow: Int
+}
+
 class WeatherDetail: WeatherLocation {
     
-    struct Result: Codable {
+    private struct Result: Codable {
         var timezone: String
         var currently: Currently
         var daily: Daily
     }
-    struct Currently: Codable {
+    private struct Currently: Codable {
         var temperature: Double
         var time: TimeInterval
     }
-    struct Daily: Codable {
+    private struct Daily: Codable {
         var summary: String
         var icon: String
+        var data: [DailyData]
+    }
+    private struct DailyData: Codable {
+        var icon: String
+        var time: TimeInterval
+        var summary: String
+        var temperatureHigh: Double
+        var temperatureLow: Double
     }
     
     var timezone = ""
@@ -29,6 +51,7 @@ class WeatherDetail: WeatherLocation {
     var temperature = 0
     var summary = ""
     var dailyIcon = ""
+    var dailyWeatherData: [DailyWeatherData] = []
     
     func getData(completed: @escaping () -> () ) {
         let coordinates = "\(latitude),\(longitude)"
@@ -59,6 +82,17 @@ class WeatherDetail: WeatherLocation {
                 self.temperature = Int(response.currently.temperature.rounded())
                 self.summary = response.daily.summary
                 self.dailyIcon = response.daily.icon
+                for index in 0..<response.daily.data.count {
+                    let weekdayDate = Date(timeIntervalSince1970: response.daily.data[index].time)
+                    dateFormatter.timeZone = TimeZone(identifier: response.timezone)
+                    let dailyWeekday = dateFormatter.string(from: weekdayDate)
+                    let dailyIcon = response.daily.data[index].icon
+                    let dailySummary = response.daily.data[index].summary
+                    let dailyHigh = Int(response.daily.data[index].temperatureHigh.rounded())
+                    let dailyLow = Int(response.daily.data[index].temperatureLow.rounded())
+                    let dailyWeather = DailyWeatherData(dailyIcon: dailyIcon, dailyWeekday: dailyWeekday, dailySummary: dailySummary, dailyHigh: dailyHigh, dailyLow: dailyLow)
+                    self.dailyWeatherData.append(dailyWeather)
+                }
             } catch {
                 print("JSON ERROR: \(error.localizedDescription)")
             }
